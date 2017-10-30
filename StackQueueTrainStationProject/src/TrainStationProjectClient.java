e149a7e0c947c74eba47ed33efec011cc19c0d17
Ian Moreno <imorenodev@gmail.com>
2017-10-30 14:09:08 -0400

    first iteration on client driver program

--- StackQueueTrainStationProject/src/TrainStationProjectClient.java
+++ StackQueueTrainStationProject/src/TrainStationProjectClient.java
@@ -18,23 +18,109 @@ public class TrainStationProjectClient {
 	public static int passengersDelivered = 0;
 	
 	public static void main(String[] args) {
+		List<Station> stationList = new ArrayList<>();
+		QueueInterface<Train> trainQueue = new LinkedQueue<>();
+		QueueInterface<Passenger> passengerQueue = new LinkedQueue<>();
+		int numberOfPassengersCreated = 0;
+		int numberOfPassengersWaiting = 0;
+
+		createStations(stationList);
 		
+		for (int time = 0; time < TIME_INTERVAL; time++) {
+			// calculate total passengers waiting
+			// output time #waiting # on Trains
+
+			// start a new train
+			startNewTrain(time, trainQueue);
+			
+			// create passengers at this interval, and update number of passengers created
+			numberOfPassengersCreated = generatePassengers(stationList, passengerQueue);
+			
+			// move the trains in the queue
+			moveTrains(trainQueue, stationList, time);
+		}
 	}
 	
-	private static void createStations() {
-		
+	/**
+	 * Purpose: private helper method createStations creates new Station objects each with a random timeToNextStation
+	 * 			and adds them to stationList.
+	 * @param stationList List<Station> that will contain the newly created Station objects.
+	 */
+	private static void createStations(List<Station> stationList) {
+		int timeToNextStation = 0;
+		Station tempStation = null;
+
+		for (int i = 0; i < NUM_STATIONS; i++) {
+			timeToNextStation = generator.nextInt(10) + 5; // a random time between 5-14 inclusive
+			tempStation = new Station(timeToNextStation);
+			stationList.add(tempStation);
+			
+			System.out.println("Station has been created with time to next station: " + timeToNextStation + " minutes.");
+		}
 	}
 	
-	private static void startNewTrain() {
-		
+	private static void startNewTrain(int time, QueueInterface<Train> trainQueue) {
+		if (time % TRAIN_INTERVAL == 0) {
+			trainQueue.enqueue(new Train(TRAIN_CAPACITY));
+			trainCount++;
+		}
 	}
 	
-	private static void generatePassengers() {
+	private static int generatePassengers(List<Station> stationList, QueueInterface<Passenger> passengerQueue) {
+		Passenger tempPassenger = null;
+		int numberOfPassengers = generator.nextInt(6);
+		int startStation = 0;
+		int stopStation = 0;
 		
+		for (int i = 0; i < numberOfPassengers; i++) {
+			while (startStation >= stopStation) {
+				startStation = generator.nextInt(NUM_STATIONS);
+				stopStation = generator.nextInt(NUM_STATIONS);
+			}
+			
+			// create new passenger
+			tempPassenger = new Passenger(startStation, stopStation);
+			
+			// add passenger to startStation's queue
+			stationList.get(startStation).addPassenger(tempPassenger);
+			
+			// add passenger to passengerQueue
+			passengerQueue.enqueue(tempPassenger);
+		}
+		return numberOfPassengers;
 	}
 	
-	private static void moveTrains() {
-		
+	private static void moveTrains(QueueInterface<Train> trainQueue, List<Station> stationList, int time) {
+		int currentTime = 0;
+		int stationNumber = 0;
+		// create temporary variable of the train count during this time interval to loop through each train
+		int tempTrainCount = trainCount;
+		Train tempTrain = null;
+
+		for (int i = 0; i < tempTrainCount; i++) {
+			tempTrain = trainQueue.dequeue();
+			tempTrain.move();
+			currentTime = tempTrain.timeToNext();
+			
+			if (currentTime == 0) {
+				stationNumber = tempTrain.nextStation();
+
+				// unload all passengers getting off at current station 
+				passengersDelivered = tempTrain.unloadPassengers(stationNumber);
+				
+				// load all passengers getting on to train from current station
+				passengersOnTrains = tempTrain.loadPassengers(stationList.get(stationNumber), time);
+				
+				// update train to next station to move to
+				tempTrain.updateStation(stationList.get(stationNumber + 1).getTimeToNextStation());
+			}
+			
+			if (tempTrain.nextStation() == 4) {
+				trainCount--;
+			} else {
+				trainQueue.enqueue(tempTrain);
+			}
+		}
 	}
 
 	/** Reports the final situations of the trains and passengers waiting
