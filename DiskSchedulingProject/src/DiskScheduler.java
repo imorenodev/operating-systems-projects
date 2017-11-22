1c4bb06e1a1fc0544d22f93f64d1449275d83499
Ian Moreno <imorenodev@gmail.com>
2017-11-22 14:44:02 -0500

    implemented SCAN and CSCAN

--- DiskSchedulingProject/src/DiskScheduler.java
+++ DiskSchedulingProject/src/DiskScheduler.java
@@ -27,10 +27,12 @@ public class DiskScheduler {
 		// assign last item in diskRequestsList as the disk request for the process
 		requestToSatisfy = diskRequestsList.get(diskRequestsList.size()-1);
 		
-		SSTF();
-		SCAN();
-		CSCAN();
-		CLOOK();
+		if (headPosition != requestToSatisfy) {
+			SSTF();
+			SCAN();
+			CSCAN();
+			CLOOK();
+		} 
 	}
 	
 	private void SSTF() {
@@ -38,22 +40,22 @@ public class DiskScheduler {
 		List<Integer> tempRequestsList = new ArrayList<>(diskRequestsList);
 		boolean satisfied = false;
 		Integer tempReq = null;
-		Integer shortestReq = null;
+		Integer nearestReq = null;
 		Integer tempHeadPos = headPosition;
 		
 		while (!satisfied) {
 			for (Integer i : tempRequestsList) {
 				tempReq = i;
 				
-				if (shortestReq == null)
-					shortestReq = tempReq;
-				else if (getDistance(tempHeadPos, tempReq) < getDistance(tempHeadPos, shortestReq))
-					shortestReq = tempReq;
+				if (nearestReq == null)
+					nearestReq = tempReq;
+				else if (getDistance(tempHeadPos, tempReq) < getDistance(tempHeadPos, nearestReq))
+					nearestReq = tempReq;
 			}
-			SSTFtotalHeadMovement += getDistance(tempHeadPos, shortestReq);
-			tempHeadPos = shortestReq;
-			tempRequestsList.remove(shortestReq);
-			shortestReq = null;
+			SSTFtotalHeadMovement += getDistance(tempHeadPos, nearestReq);
+			tempHeadPos = nearestReq;
+			tempRequestsList.remove(nearestReq);
+			nearestReq = null;
 			
 			if (tempHeadPos.intValue() == requestToSatisfy)
 				satisfied = true;
@@ -63,55 +65,243 @@ public class DiskScheduler {
 	private void SCAN() {
 		// temporary request list to mutate
 		List<Integer> tempRequestsList = new ArrayList<>(diskRequestsList);
+		// sort reqs list
+		Collections.sort(tempRequestsList);
 		boolean satisfied = false;
-		Integer tempReq = null;
-		Integer shortestReq = null;
-		Integer tempHeadPos = headPosition;
-		
+		// initialize nearest largest req to the last index of the list in case the head position is greater than all the reqs in the list
+		Integer lastReqInList = tempRequestsList.get(tempRequestsList.size()-1); 
+		Integer upperBound = null;
+		Integer lowerBound = null;
+		Integer tempHeadPos = new Integer(headPosition);
+		int indexOfUpperBound = 0;
+	
+
 		while (!satisfied) {
-			for (Integer i : tempRequestsList) {
-				tempReq = i;
-				
-				if (shortestReq == null)
-					shortestReq = tempReq;
-				else if (getDistance(tempHeadPos, tempReq) < getDistance(tempHeadPos, shortestReq))
-					shortestReq = tempReq;
+			if (tempHeadPos > lastReqInList) {
+				// head is positioned past the last/largest req in the list so set lowerBound to the last element in list
+				lowerBound = lastReqInList;
+				upperBound = Integer.valueOf(500);
+			} else { // find the upper bound req relative to the head position
+				while (indexOfUpperBound < tempRequestsList.size()) {
+					if (tempHeadPos < tempRequestsList.get(indexOfUpperBound)) {
+						upperBound = new Integer(tempRequestsList.get(indexOfUpperBound));
+						
+						if (indexOfUpperBound > 0) 
+							lowerBound = new Integer(tempRequestsList.get(indexOfUpperBound-1));
+						else
+							lowerBound = new Integer(0);
+
+						break;
+					}
+					indexOfUpperBound++;
+				}
 			}
-			SCANtotalHeadMovement += getDistance(tempHeadPos, shortestReq);
-			tempHeadPos = shortestReq;
-			tempRequestsList.remove(shortestReq);
-			shortestReq = null;
-			
-			if (tempHeadPos.intValue() == requestToSatisfy)
-				satisfied = true;
+
+			if (getDistance(tempHeadPos, lowerBound) < getDistance(tempHeadPos, upperBound)) {
+				// go left
+				for (int i = tempRequestsList.indexOf(lowerBound)-1; i > 0; i--) {
+					SCANtotalHeadMovement += getDistance(tempHeadPos, lowerBound);
+					tempHeadPos = new Integer(lowerBound);
+					//tempRequestsList.remove(lowerBound);
+					lowerBound = new Integer(tempRequestsList.get(i));
+					if (tempHeadPos.intValue() == requestToSatisfy)
+						return;
+				}
+				SCANtotalHeadMovement += getDistance(tempHeadPos, lowerBound);
+				tempHeadPos = new Integer(lowerBound);
+				//tempRequestsList.remove(lowerBound);
+				lowerBound = new Integer(0);
+				if (tempHeadPos.intValue() == requestToSatisfy)
+					return;
+
+				SCANtotalHeadMovement += getDistance(tempHeadPos, lowerBound);
+				tempHeadPos = new Integer(0);
+				upperBound = new Integer(tempRequestsList.get(indexOfUpperBound));
+				if (tempHeadPos.intValue() == requestToSatisfy)
+					return;
+
+				// at 0 now go right
+				for (int j = indexOfUpperBound+1; j < tempRequestsList.size()-1; j++) {
+					SCANtotalHeadMovement += getDistance(tempHeadPos, upperBound);
+					tempHeadPos = new Integer(upperBound);
+					upperBound = new Integer(tempRequestsList.get(j));
+					if (tempHeadPos.intValue() == requestToSatisfy)
+						return;
+				}
+				SCANtotalHeadMovement += getDistance(tempHeadPos, upperBound);
+				tempHeadPos = new Integer(upperBound);
+				upperBound = Integer.valueOf(500);
+				if (tempHeadPos.intValue() == requestToSatisfy)
+					return;			
+				
+				SCANtotalHeadMovement += getDistance(tempHeadPos, upperBound);
+				
+			} else {
+				// go right
+				for (int i = tempRequestsList.indexOf(upperBound)+1; i < tempRequestsList.size()-1; i++) {
+					SCANtotalHeadMovement += getDistance(tempHeadPos, upperBound);
+					tempHeadPos = new Integer(upperBound);
+					//tempRequestsList.remove(upperBound);
+					upperBound = new Integer(tempRequestsList.get(i));
+					if (tempHeadPos.intValue() == requestToSatisfy)
+						return;
+				}
+				SCANtotalHeadMovement += getDistance(tempHeadPos, upperBound);
+				tempHeadPos = new Integer(upperBound);
+				//tempRequestsList.remove(upperBound);
+				upperBound = new Integer(500);
+				if (tempHeadPos.intValue() == requestToSatisfy)
+					return;
+
+				SCANtotalHeadMovement += getDistance(tempHeadPos, upperBound);
+				tempHeadPos = new Integer(500);
+				lowerBound = new Integer(tempRequestsList.get(indexOfUpperBound-1));
+				if (tempHeadPos.intValue() == requestToSatisfy)
+					return;
+
+				// at 5000 now go left 
+				for (int j = indexOfUpperBound-2; j >= 0; j--) {
+					SCANtotalHeadMovement += getDistance(tempHeadPos, lowerBound);
+					tempHeadPos = new Integer(lowerBound);
+					lowerBound = new Integer(tempRequestsList.get(j));
+					if (tempHeadPos.intValue() == requestToSatisfy)
+						return;
+				}
+				SCANtotalHeadMovement += getDistance(tempHeadPos, lowerBound);
+				tempHeadPos = new Integer(lowerBound);
+				lowerBound = Integer.valueOf(0);
+				if (tempHeadPos.intValue() == requestToSatisfy)
+					return;			
+				
+				SCANtotalHeadMovement += getDistance(tempHeadPos, lowerBound);	
+			}
+			return;
 		}
 	}
 	
 	private void CSCAN() {
 		// temporary request list to mutate
 		List<Integer> tempRequestsList = new ArrayList<>(diskRequestsList);
+		// sort reqs list
+		Collections.sort(tempRequestsList);
 		boolean satisfied = false;
-		Integer tempReq = null;
-		Integer shortestReq = null;
-		Integer tempHeadPos = headPosition;
-		
+		// initialize nearest largest req to the last index of the list in case the head position is greater than all the reqs in the list
+		Integer lastReqInList = tempRequestsList.get(tempRequestsList.size()-1); 
+		Integer upperBound = null;
+		Integer lowerBound = null;
+		Integer tempHeadPos = new Integer(headPosition);
+		int indexOfUpperBound = 0;
+	
+
 		while (!satisfied) {
-			for (Integer i : tempRequestsList) {
-				tempReq = i;
-				
-				if (shortestReq == null)
-					shortestReq = tempReq;
-				else if (getDistance(tempHeadPos, tempReq) < getDistance(tempHeadPos, shortestReq))
-					shortestReq = tempReq;
+			if (tempHeadPos > lastReqInList) {
+				// head is positioned past the last/largest req in the list so set lowerBound to the last element in list
+				lowerBound = lastReqInList;
+				upperBound = Integer.valueOf(500);
+			} else { // find the upper bound req relative to the head position
+				while (indexOfUpperBound < tempRequestsList.size()) {
+					if (tempHeadPos < tempRequestsList.get(indexOfUpperBound)) {
+						upperBound = new Integer(tempRequestsList.get(indexOfUpperBound));
+						
+						if (indexOfUpperBound > 0) 
+							lowerBound = new Integer(tempRequestsList.get(indexOfUpperBound-1));
+						else
+							lowerBound = new Integer(0);
+
+						break;
+					}
+					indexOfUpperBound++;
+				}
 			}
-			CSCANtotalHeadMovement += getDistance(tempHeadPos, shortestReq);
-			tempHeadPos = shortestReq;
-			tempRequestsList.remove(shortestReq);
-			shortestReq = null;
-			
-			if (tempHeadPos.intValue() == requestToSatisfy)
-				satisfied = true;
-		}
+
+			if (getDistance(tempHeadPos, lowerBound) < getDistance(tempHeadPos, upperBound)) {
+				// go left
+				for (int i = tempRequestsList.indexOf(lowerBound)-1; i > 0; i--) {
+					CSCANtotalHeadMovement += getDistance(tempHeadPos, lowerBound);
+					tempHeadPos = new Integer(lowerBound);
+					//tempRequestsList.remove(lowerBound);
+					lowerBound = new Integer(tempRequestsList.get(i));
+					if (tempHeadPos.intValue() == requestToSatisfy)
+						return;
+				}
+				CSCANtotalHeadMovement += getDistance(tempHeadPos, lowerBound);
+				tempHeadPos = new Integer(lowerBound);
+				//tempRequestsList.remove(lowerBound);
+				lowerBound = new Integer(0);
+				if (tempHeadPos.intValue() == requestToSatisfy)
+					return;
+
+				CSCANtotalHeadMovement += getDistance(tempHeadPos, lowerBound);
+				if (tempHeadPos.intValue() == requestToSatisfy)
+					return;
+
+				// at 0 now go to end and conitnue to go left
+
+				tempHeadPos = new Integer(500);
+				lowerBound = new Integer(tempRequestsList.get(tempRequestsList.size()-1));
+				
+				// go left
+				for (int i = tempRequestsList.indexOf(lowerBound)-1; i > indexOfUpperBound; i--) {
+					CSCANtotalHeadMovement += getDistance(tempHeadPos, lowerBound);
+					tempHeadPos = new Integer(lowerBound);
+					//tempRequestsList.remove(lowerBound);
+					lowerBound = new Integer(tempRequestsList.get(i));
+					if (tempHeadPos.intValue() == requestToSatisfy)
+						return;
+				}
+
+				CSCANtotalHeadMovement += getDistance(tempHeadPos, lowerBound);
+				tempHeadPos = new Integer(lowerBound);
+				//tempRequestsList.remove(lowerBound);
+				lowerBound = new Integer(tempRequestsList.get(indexOfUpperBound));
+				if (tempHeadPos.intValue() == requestToSatisfy)
+					return;
+
+				CSCANtotalHeadMovement += getDistance(tempHeadPos, lowerBound);
+			} else {
+				// go right
+
+				for (int i = tempRequestsList.indexOf(upperBound)+1; i < tempRequestsList.size()-1; i++) {
+					CSCANtotalHeadMovement += getDistance(tempHeadPos, upperBound);
+					tempHeadPos = new Integer(upperBound);
+					upperBound = new Integer(tempRequestsList.get(i));
+					if (tempHeadPos.intValue() == requestToSatisfy)
+						return;
+				}
+				CSCANtotalHeadMovement += getDistance(tempHeadPos, upperBound);
+				tempHeadPos = new Integer(upperBound);
+				upperBound = new Integer(500);
+				if (tempHeadPos.intValue() == requestToSatisfy)
+					return;
+
+				CSCANtotalHeadMovement += getDistance(tempHeadPos, upperBound);
+				if (tempHeadPos.intValue() == requestToSatisfy)
+					return;
+
+				// at 500 now go to beginning and continue to go right 
+
+				tempHeadPos = new Integer(0);
+				upperBound = new Integer(tempRequestsList.get(0));
+				
+				// go right 
+				for (int i = tempRequestsList.indexOf(upperBound)+1; i < indexOfUpperBound; i++) {
+					CSCANtotalHeadMovement += getDistance(tempHeadPos, upperBound);
+					tempHeadPos = new Integer(upperBound);
+					upperBound = new Integer(tempRequestsList.get(i));
+					if (tempHeadPos.intValue() == requestToSatisfy)
+						return;
+				}
+
+				CSCANtotalHeadMovement += getDistance(tempHeadPos, upperBound);
+				tempHeadPos = new Integer(upperBound);
+				upperBound = new Integer(tempRequestsList.get(indexOfUpperBound));
+				if (tempHeadPos.intValue() == requestToSatisfy)
+					return;
+
+				CSCANtotalHeadMovement += getDistance(tempHeadPos, upperBound);	
+			}
+			return;
+		}	
 	}
 	
 	private void CLOOK() {
@@ -119,30 +309,35 @@ public class DiskScheduler {
 		List<Integer> tempRequestsList = new ArrayList<>(diskRequestsList);
 		boolean satisfied = false;
 		Integer tempReq = null;
-		Integer shortestReq = null;
+		Integer nearestReq = null;
 		Integer tempHeadPos = headPosition;
 		
 		while (!satisfied) {
 			for (Integer i : tempRequestsList) {
 				tempReq = i;
 				
-				if (shortestReq == null)
-					shortestReq = tempReq;
-				else if (getDistance(tempHeadPos, tempReq) < getDistance(tempHeadPos, shortestReq))
-					shortestReq = tempReq;
+				if (nearestReq == null)
+					nearestReq = tempReq;
+				else if (getDistance(tempHeadPos, tempReq) < getDistance(tempHeadPos, nearestReq))
+					nearestReq = tempReq;
 			}
-			CLOOKtotalHeadMovement += getDistance(tempHeadPos, shortestReq);
-			tempHeadPos = shortestReq;
-			tempRequestsList.remove(shortestReq);
-			shortestReq = null;
+			CLOOKtotalHeadMovement += getDistance(tempHeadPos, nearestReq);
+			tempHeadPos = nearestReq;
+			tempRequestsList.remove(nearestReq);
+			nearestReq = null;
 			
 			if (tempHeadPos.intValue() == requestToSatisfy)
 				satisfied = true;
 		}
 	}
 	
-	private int getDistance(int a, int b) {
-		return Math.abs(a - b);
+	private int getDistance(Integer a, Integer b) {
+		if (a == null || b == null) {
+			JOptionPane.showMessageDialog(null,  "Null passed to getDistance");
+		} else {
+			return Math.abs(a - b);
+		}
+		return 0;
 	}
 
 	private void buildRandomRequestsList() {
@@ -180,6 +375,7 @@ public class DiskScheduler {
 		for (int i = 0; i < 4; i++) {
 			str.append("**" + algorithms[i] + "**\tRequest List: " + diskRequestsList);
 			str.append("  Head Position: " + headPosition);
+			str.append("  Request to Satisfy: " + requestToSatisfy);
 			str.append("\tTotal Head Movement: " + headMovements[i] + "\n");
 		}
 
